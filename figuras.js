c120ee758e94566e8957c567afde25edaa092a6f
Uncletetsuo <fer.cst96@gmail.com>
2021-11-20 15:41:52 -0600

    empezando reto a ver a donde me lleva

--- figuras.js
+++ figuras.js
@@ -97,3 +97,21 @@ function areaCirculo(radio) {
 //     + "cm^2"
 // );
 // console.groupEnd();
+
+
+// interaccion con HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro)
+}
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area)
+
+}
